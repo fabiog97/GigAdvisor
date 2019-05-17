3aad1bf30fcabcb061aca52668250e4854a15800
Fabio <fabiog97@gmail.com>
2019-05-17 10:44:44 +0200

    Aggiunta Home con Slide Show

--- GigAdvisor/forms.py
+++ GigAdvisor/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.forms import Textarea
 from .models import Recensioni
 
 
@@ -7,4 +7,9 @@ class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = Recensioni
-        fields = ('titolo','descrizione')
+        fields = ['titolo', 'descrizione']
+        widgets = {
+            'titolo': Textarea(attrs={'cols': 60, 'rows': 1}),
+            'descrizione': Textarea(attrs={'cols': 60, 'rows': 8}),
+
+        }
