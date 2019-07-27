20236db5cdbdf2d12dd202b47293b0899b92f64a
Fabio <fabiog97@gmail.com>
2019-07-27 20:28:02 +0200

    Modifica Grafici

--- GigAdvisor/urls.py
+++ GigAdvisor/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('platforms/', views.PlatformListView.as_view(), name='platforms'),
     #path('platforms/new/', views.recensione_new, name='recensioni_new'),
-    path('platforms/<int:id>/', views.recensione_new, name='recensioni_new'),
+    path('platforms/new_review/<int:id>/', views.recensione_new, name='recensioni_new'),
     path('platforms/reviews/<int:id>/', views.recensione_platform, name='recensione_platform'),
+    path('platforms/reviews/andamento/<int:id>/', views.andamento_platform, name='andamento'),
 ]
--- GigAdvisor/views.py
+++ GigAdvisor/views.py
@@ -3,12 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from static.fusioncharts import FusionCharts
+from static.fusioncharts import FusionTable
+from static.fusioncharts import TimeSeries
 from collections import OrderedDict
 from django.shortcuts import render
 from GigAdvisor.forms import ReviewForm
 from .models import *
+from django.core import serializers
 import simplejson
+import requests
 
+
+import json
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
@@ -57,12 +63,14 @@ def charts(request):
     dataSource1['data'] = []
 
     for plat in Platform.objects.all():
-        for key in Recensioni.objects.filter(platform__id=plat.id):
-            data = {}
-
-            data['label'] = plat.nome
-            data['value'] = simplejson.dumps(key.value1, use_decimal=True)
-            dataSource1['data'].append(data)
+        data = {}
+        avg_value1 = Recensioni.objects.filter(platform__id=plat.id).aggregate(Avg('value1')).get('value1__avg')
+        
+        data['label'] = plat.nome
+        data['value'] = int(avg_value1)
+        dataSource1['data'].append(data)
+        avg_value1 = 0
+        
 
 
 
@@ -78,12 +86,11 @@ def charts(request):
     dataSource2['data'] = []
 
     for plat in Platform.objects.all():
-        for key in Recensioni.objects.filter(platform__id=plat.id):
-            data = {}
-
-            data['label'] = plat.nome
-            data['value'] = simplejson.dumps(key.value2, use_decimal=True)
-            dataSource2['data'].append(data)
+        data = {}
+        avg_value2 = Recensioni.objects.filter(platform__id=plat.id).aggregate(Avg('value2')).get('value2__avg')
+        data['label'] = plat.nome
+        data['value'] = int(avg_value2)
+        dataSource2['data'].append(data)
 
     dataSource3 = {}
     dataSource3['chart'] = {
@@ -97,12 +104,11 @@ def charts(request):
     dataSource3['data'] = []
 
     for plat in Platform.objects.all():
-        for key in Recensioni.objects.filter(platform__id=plat.id):
-            data = {}
-
-            data['label'] = plat.nome
-            data['value'] = simplejson.dumps(key.value3, use_decimal=True)
-            dataSource3['data'].append(data)
+        data = {}
+        avg_value3 = Recensioni.objects.filter(platform__id=plat.id).aggregate(Avg('value3')).get('value3__avg')
+        data['label'] = plat.nome
+        data['value'] = int(avg_value3)
+        dataSource3['data'].append(data)
 
     dataSource4 = {}
     dataSource4['chart'] = {
@@ -116,21 +122,19 @@ def charts(request):
     dataSource4['data'] = []
 
     for plat in Platform.objects.all():
-        for key in Recensioni.objects.filter(platform__id=plat.id):
-            data = {}
-
-            data['label'] = plat.nome
-            data['value'] = simplejson.dumps(key.value4, use_decimal=True)
-            dataSource4['data'].append(data)
-
-
+        data = {}
+        avg_value4 = Recensioni.objects.filter(platform__id=plat.id).aggregate(Avg('value4')).get('value4__avg')
+        data['label'] = plat.nome
+        data['value'] = int(avg_value4)
+        dataSource4['data'].append(data)
 
     column2D = FusionCharts("column2d", "myFirstChart", "100%", "400", "chart-container", "json", dataSource)
+
     graph1 = FusionCharts("column2d", "myFirstChart1", "100%", "300", "chart1-container", "json", dataSource1)
     graph2 = FusionCharts("column2d", "myFirstChart2", "100%", "300", "chart2-container", "json", dataSource2)
     graph3 = FusionCharts("column2d", "myFirstChart3", "100%", "300", "chart3-container", "json", dataSource3)
     graph4 = FusionCharts("column2d", "myFirstChart4", "100%", "300", "chart4-container", "json", dataSource4)
-    
+        
     return render(request, 'charts.html', {'output': column2D.render(), 'output1': graph1.render(), 'output2': graph2.render(), 'output3': graph3.render(), 'output4': graph4.render()})
 
 def home(request):
@@ -181,9 +185,62 @@ def success(request):
 
 
 
+def andamento_platform (request, id):
+
+    schema_chart = [{
+        "name": "Time",
+        "type": "date",
+        "format": "%d-%b-%y"
+        }, {
+        "name": "Type",
+        "type": "string"
+        }, {
+        "name": "Sales Value",
+        "type": "number"
+        }]
+    funds = Recensioni.objects.filter(platform__id=id).all()
+    data = serializers.serialize("json", funds, fields=('data','value1','value2','value3', 'value4',))
+    
+    y = json.loads(data)
+    
+    final_data = []
+    from datetime import datetime
+    import pytz
+    
+    for i in range(len(y)):
+        z = y[i]
+        data = datetime.strptime(z['fields']['data'], "%Y-%m-%dT%H:%M:%S.%f%z")
+        data = data.strftime("%d-%b-%y")
+        d1 = [data, "Sicurezza sul lavoro", z['fields']['value1']]
+        d2 = [data, "Trasparenza del contratto", z['fields']['value2']]
+        d3 = [data, "Tempestività dei pagamenti", z['fields']['value3']]
+        d4 = [data, "Correttezza del datore di lavoro", z['fields']['value4']]
+        final_data.append(d1)
+        final_data.append(d2)
+        final_data.append(d3)
+        final_data.append(d4)
+
+    fusionTable = FusionTable(str(schema_chart), str(final_data))
+    
+    timeSeries = TimeSeries(fusionTable)
+
+    timeSeries.AddAttribute('chart', '{}')
+    timeSeries.AddAttribute('caption', '{"text":"Sales Analysis"}')
+    timeSeries.AddAttribute('subcaption', '{"text":"Grocery & Footwear"}')
+    timeSeries.AddAttribute('series', '"Type"')
+    timeSeries.AddAttribute('yaxis', '[{"plot":"Sales Value","title":"Sale Value","format":{"prefix":""}}]')
+    
+
+    # Create an object for the chart using the FusionCharts class constructor
+    fcChart = FusionCharts("timeseries", "ex1", 700, 450, "chart-1", "json", timeSeries)
+
+    
+    return render(request, 'andamento_platform.html', {'output5': fcChart.render()})
+
+
 def recensione_platform (request, id):
     platform = Platform.objects.get(id=id)
-
+    
     recensioni = Recensioni.objects.filter(platform__id=platform.id).values()
     avg_value1 = Recensioni.objects.filter(platform__id=platform.id).aggregate(Avg('value1'))
     avg_value2 = Recensioni.objects.filter(platform__id=platform.id).aggregate(Avg('value2'))
