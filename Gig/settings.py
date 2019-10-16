8530c26e8c328ed986108d0d175b1767926109d6
Fabio <fabiog97@gmail.com>
2019-10-16 12:24:16 +0200

    Change MEDIA_ROOT (Project_Root)

--- Gig/settings.py
+++ Gig/settings.py
@@ -192,7 +192,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
 
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
