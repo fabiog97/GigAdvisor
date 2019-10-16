40364b9ce7f9ed445f60f1b3633c5277390f5dbb
Fabio <fabiog97@gmail.com>
2019-10-16 12:36:39 +0200

    Add staticfiles directory

--- Gig/settings.py
+++ Gig/settings.py
@@ -192,7 +192,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
