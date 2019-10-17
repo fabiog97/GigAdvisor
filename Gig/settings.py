0727bec464a83293f9889526fe8c9802d95c623a
Fabio <fabiog97@gmail.com>
2019-10-17 14:34:52 +0200

    Change DB setting

--- Gig/settings.py
+++ Gig/settings.py
@@ -148,15 +148,16 @@ DATABASES = {
         'PASSWORD': 'xW0reilctF',
         'HOST': 'remotemysql.com',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
+        'CONN_MAX_AGE': 100,
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=100)
 
 DATABASES['default'] = dj_database_url.config(default='mysql://Efw8jMHvhc:xW0reilctF@remotemysql.com:3306/Efw8jMHvhc')
 
 DATABASES['default'] = dj_database_url.parse('mysql://Efw8jMHvhc:xW0reilctF@remotemysql.com:3306/Efw8jMHvhc', conn_max_age=100)
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=100, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
